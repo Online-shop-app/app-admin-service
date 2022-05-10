9f14149583cb19f8a433a4ff58393f93a112fb86
Rustam Mamedov <alwaysontheroad.rb@gmail.com>
2022-05-10 17:12:08 +0300

    added: docker image collaboration plugin, prometheus metrics api

--- src/main/java/com/example/springadminserver/SpringAdminServerApplication.java
+++ src/main/java/com/example/springadminserver/SpringAdminServerApplication.java
@@ -3,12 +3,18 @@ package com.example.springadminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class SpringAdminServerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(SpringAdminServerApplication.class, args);
     }
 }
+
